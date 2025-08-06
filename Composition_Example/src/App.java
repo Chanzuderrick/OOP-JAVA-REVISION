775fb678e6f553d17496410989dbec03d1662941
Chanzuderrick <137392204+Chanzuderrick@users.noreply.github.com>
2025-08-06 16:58:27 +0300

    composition changes

--- Composition_Example/src/App.java
+++ Composition_Example/src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*
+        Composition
+        it represents a part of relationship between objects
+        for example an engine is a part of a car
+        allows complex objects to be constructed from smaller objects
+
+
+
+        */
+
+        //if I delete the car class then it will be affected
+
+        Car car = new Car("Corvetter", 2023,"v8");
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.engine.type);
+        car.start();
     }
 }
